4a86dd742461108825acc975eadf0afd03444280
kelsey nocek <kelseyn12@hotmail.com>
2022-01-30 22:19:06 -0600

    worked on nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ import '../styles.css';
 
 function Header(props) {
     return (
-        <ul className='ulHeader' style={{display: 'flex', listStyle:'none', justifyContent: 'space-around'}}>
-            <li><Link activeClass='active' to="about" spy={true} smooth={true}>About</Link></li>
+        <ul className='ulHeader'>
+         <div className='alignleft'> <h2 className='headerName'>Kelsey Nocek</h2> </div>  
+          <div className='alignright'> <li><Link activeClass='active' to="about" spy={true} smooth={true}>About</Link></li>
             <li><Link to="projects" spy={true} smooth={true}>Projects</Link></li>
             <li><Link to="holla" spy={true} smooth={true}>Holla!</Link></li>
+            </div>
+            <div style={{clear: "both"}}> </div>
         </ul>
     )
 }
