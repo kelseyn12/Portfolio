ad5fe6075521d657459b89d9ade45bc695fad2ad
kelsey nocek <kelseyn12@hotmail.com>
2022-03-10 16:28:43 -0600

    added a carousel to projects

--- src/components/MiddleSection.js
+++ src/components/MiddleSection.js
@@ -60,7 +60,6 @@ function MiddleSection() {
         <h2 id="h2_about"> Designer & Developer.</h2>
         <img
           src="https://i.ibb.co/ZLJ3718/000036570019.jpg"
-          // "https://i.ibb.co/DgjpY3G/000066010028.jpg"
           alt="Kelsey's face"
           className="kelseysFace"
         />
@@ -73,34 +72,36 @@ function MiddleSection() {
       </div>
       <div id="projects_page" style={{ height: 900 }}>
         <h1 className="projects-h1">Projects</h1>
-        <div className="container">
-          <div className="projects">
-            <a href="https://kelseyn12.github.io/Surf-App-Project/" rel="noreferrer" target="_blank">
-            <img
-              src="https://i.ibb.co/TgWSStz/146481246-8b9ca05b-70ff-49d6-a894-45e378c92fad.png"
-              alt="Surf application"
-              className="image-1"
-            />
-            </a>
-            <a href="https://hello-project.netlify.app/" rel="noreferrer" target="_blank">
-            <img
-              src="https://i.ibb.co/tZbSwfT/Screen-Shot-2022-02-06-at-6-37-13-PM.png"
-              alt="Project idea application"
-              className="image-2"
-            />
-            </a>
+        
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <a href="https://kelseyn12.github.io/Surf-App-Project/" rel="noreferrer" target="_blank">
+      <img class="d-block w-100 carousel-image" src="https://i.ibb.co/TgWSStz/146481246-8b9ca05b-70ff-49d6-a894-45e378c92fad.png" alt="First slide" />
+      </a>
+    </div>
+    <div class="carousel-item">
+    <a href="https://hello-project.netlify.app/" rel="noreferrer" target="_blank">
+      <img class="d-block w-100 carousel-image" src="https://i.ibb.co/tZbSwfT/Screen-Shot-2022-02-06-at-6-37-13-PM.png" alt="Second slide" />
+    </a>
+    </div>
+    <div class="carousel-item">
+    <a href="https://midwest-bikepacking.herokuapp.com/" rel="noreferrer" target="_blank">
+      <img class="d-block w-100 carousel-image" src="https://i.ibb.co/Zm4z2qY/Screen-Shot-2022-03-10-at-3-12-50-PM.png" alt="Third slide" />
+    </a>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" >
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" >
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
             
-            <a href="https://midwest-bikepacking.herokuapp.com/" rel="noreferrer" target="_blank">
-            <img
-              src="https://i.ibb.co/Q8hkJXw/149541560-63144e6f-70c0-4fbd-86f5-5cb94a62243b.png"
-              alt="Bikepacker Board Website"
-              className="image-3"
-              
-            />
-            </a>
-            
-          </div>
-        </div>
+          
         <h2 className="more"> More to come!</h2>
       </div>
       <div id="holla" style={{ height: 600 }}>
